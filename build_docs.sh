e95ec9241db3d10df079b7fbdd7c2344e078e271
Yaroslav Litvinov <yaroslav.litvinov@gmail.com>
2020-10-29 15:54:16 +0200

    change in build docs

--- build_docs.sh
+++ build_docs.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-git checkout master && \
-	git pull origin master && \
+branch=${master:-$1}
+
+git checkout $branch && \
+	git pull origin $branch && \
 	( git branch -D docs || true ) && \
 	git checkout -b docs && \
-	docma && \
+	yarn docs && \
 	git add docs && \
 	git commit -m "docs" && \
-git checkout master
-
+git checkout $branch
